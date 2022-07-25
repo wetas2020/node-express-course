919f68c0e8a09da89269549a9d8bc5714726fb63
Saad El Gabouri <elgabouri.saad@gmail.com>
2022-07-25 22:50:26 +0100

    add first GET route

--- server.js
+++ server.js
@@ -3,6 +3,19 @@ const express = require('express');
 // assign the express module to the app variable
 const app = express();
 
+// create dammy data
+const mockUserData = [{ name: 'Saad' }, { name: 'Haitam' }];
+
+// get the user data
+app.get('/users', function (req, res) {
+    res.json({
+        success: true,
+        message: 'Users retrieved successfully',
+        users: mockUserData
+    });
+});
+
+// start the server
 app.listen(8000, function () {
     console.log('listening on port 8000');
 });
