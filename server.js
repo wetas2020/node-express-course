5e4e1c7d13c377eb19decd5cf879126b8a65d269
Saad El Gabouri <elgabouri.saad@gmail.com>
2022-07-25 22:44:33 +0100

    set up express server

---
+++ server.js
@@ -0,0 +1,8 @@
+// import the express module
+const express = require('express');
+// assign the express module to the app variable
+const app = express();
+
+app.listen(8000, function () {
+    console.log('listening on port 8000');
+});
