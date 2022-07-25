c76c5b26d0e32cc95f0adc94128b05930affaf99
Saad El Gabouri <elgabouri.saad@gmail.com>
2022-07-25 23:04:02 +0100

    add login POST route

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 // import the express module
 const express = require('express');
+const bodyParser = require('body-parser');
 // assign the express module to the app variable
 const app = express();
+app.use(bodyParser.json());
 
 // create dummy-data
 const mockUserData = [{ name: 'Saad' }, { name: 'Haitam' }];
@@ -15,6 +17,31 @@ app.get('/users/:id', function (req, res) {
         message: 'User data retrieved successfully',
         users: req.params.id
     });
+});
+
+// post the user data to login endpoint
+app.post('/login', function (req, res) {
+    // get the user data from the request
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // check if the user data is valid
+    // this should be come from the database
+    const mockUsername = 'Wetas';
+    const mockPassword = 'superSecret';
+
+    if (username === mockUsername && password === mockPassword) {
+        res.json({
+            success: true,
+            message: 'User Data Matched!',
+            token: 'encrypted Token here'
+        });
+    } else {
+        res.json({
+            success: false,
+            message: 'User Data Not Matched!'
+        });
+    }
 });
 
 // start the server
